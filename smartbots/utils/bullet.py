05a559316b40982d21125add94aa2434a1316dfc
alex <cuatroalejandro@gmail.com>
2021-08-21 23:48:33 -0400

    update bullet utils

--- smartbots/utils/bullet.py
+++ smartbots/utils/bullet.py
@@ -21,3 +21,27 @@ def load_urdf(urdf=''):
         This function load the urdf
     """
     p.loadURDF(urdf)
+
+def setup_init(timestep=1./240,k iterations=150, gravity=-9.5):
+    """ 
+        This function give an initial  setup to the reset
+    """
+    p.setPhysicsEngineParameter(numSolverIterations=iterations)
+    p.setTimeStep(timeStep)
+    p.setGravity(0, 0, gravity)
+    p.setRealTimeSimulation(False)
+    p.stepSimulation()
+
+def reset_robot(id, reset_joint_indices, reset_joint_values):
+
+    """
+        This function reset all the states of the joints of the robot 
+    """
+    assert len(reset_joint_indices) == len(reset_joint_values)
+    for i, value in zip(reset_joint_indices, reset_joint_values):
+        p.resetJointState(id, i, value)
+
+
+
+    
+
