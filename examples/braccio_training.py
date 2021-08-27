7b12f983f14fb83bb6a8f2af3589fe0c6a91f83e
alex <cuatroalejandro@gmail.com>
2021-08-26 23:15:17 -0400

    update braccio_training
    checking reset function

--- examples/braccio_training.py
+++ examples/braccio_training.py
@@ -1,12 +1,13 @@
-import gym 
+import gym
 import smartbots
 
 def main():
-    env = gym.make('Bracciok-v0')
+    env = gym.make('Braccio-v0')
     env.reset()
     for _ in range(1000):
-        env.render()
-        env.step(env.action_space.sample())
+        print(env.reset())
+        # env.render()
+        # env.step(env.action_space.sample())
     env.close()
 
 if __name__ == '__main__':
