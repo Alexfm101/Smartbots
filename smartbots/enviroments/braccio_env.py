3f337116dd01f6d8f283c12dba4b3a29bbd3180c
alex <cuatroalejandro@gmail.com>
2021-08-26 23:16:05 -0400

    update reset function and add new self variables

--- smartbots/enviroments/braccio_env.py
+++ smartbots/enviroments/braccio_env.py
@@ -2,12 +2,13 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 import pybullet as p
+import pybullet_data
 import smartbots.utils.bullet as bullet
 
 # VARIABLES INICIALES
 #------------------------------
-RESET_JOINT_VALUES = [1.57, -0.6, 0, -1.57, 0, 0, 0.5]
-RESET_JOINT_INDICES = [0, 1, 2, 3, 4, 5, 6]
+RESET_JOINT_VALUES = [1.57, 0.6, 0., 1.57, 0., 0., 0.5,0.]
+RESET_JOINT_INDICES = [0, 1, 2, 3, 4, 5, 6,7]
 
 # AMBIENTE DEL BRACCIO
 # -----------------------------
@@ -15,10 +16,23 @@ RESET_JOINT_INDICES = [0, 1, 2, 3, 4, 5, 6]
 class BraccioEnv(gym.Env):
     metadata = {'render.modes':['human']}
 
-    def __init__(self):
-        self.reset_joint_indices = RESET_JOINT_VALUES
-        self.reset_joint_values = RESET_JOINT_INDICES
-        self.id = 1
+    def __init__(self,gui=True):
+
+        self.reset_joint_indices = RESET_JOINT_INDICES
+        self.reset_joint_values = RESET_JOINT_VALUES
+        self.gui = gui
+
+        bullet.connectSimulation(gui=self.gui)
+
+        self.id =  p.loadURDF('./smartbots/assets/Manipulators/braccio.urdf',
+                        basePosition=[0,0,0],
+                        baseOrientation=p.getQuaternionFromEuler([0,0,0])
+                        )
+        p.loadURDF("plane.urdf") #TODO:agregar piso
+        self.indices = list(range(0,p.getNumJoints(self.id)))
+
+
+
 
     def step(self,action):
         # TODO:
@@ -32,9 +46,6 @@ class BraccioEnv(gym.Env):
         return observation, reward, done, info
 
     def reset(self):
-        # resetear simulacion
-        p.setRealTimeSimulation(False)
-        p.resetSimulation()
 
         # dar las configuraciones iniciales del ambiente
         bullet.setup_init()
@@ -45,11 +56,16 @@ class BraccioEnv(gym.Env):
             self.reset_joint_indices,
             self.reset_joint_values
         )
-        # TODO:crear robot_states
+
+        # generar la observacion del ambiente
+        braccio_joints_positions, braccio_joints_velocities = bullet.jointStates(self.id, self.indices)
+
         # observacion del ambiente para el agente
         observation = {
-            "braccio_states": robot_states
+            "braccio_joints_positions": braccio_joints_positions,
+            "braccio_joints_velocities": braccio_joints_velocities
         }
+
         return observation
 
     def render(self, mode='human'):
